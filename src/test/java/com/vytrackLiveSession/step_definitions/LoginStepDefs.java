61328a082b6021b7ec65eaadaef764db62b05903
Evelin <evelindacanovas@gmail.com>
2021-08-07 17:49:00 +0200

    feature and steps created in login.feature steps definitions done in created LoginStepDefs

---
+++ src/test/java/com/vytrackLiveSession/step_definitions/LoginStepDefs.java
@@ -0,0 +1,19 @@
+package com.vytrackLiveSession.step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class LoginStepDefs {
+    @Given("the user is on the login page")
+    public void theUserIsOnTheLoginPage() {
+    }
+
+    @When("the user enters the driver information")
+    public void theUserEntersTheDriverInformation() {
+    }
+
+    @Then("the user sould be able to login")
+    public void theUserSouldBeAbleToLogin() {
+    }
+}
---
+++ src/test/resources/features/login.feature
@@ -0,0 +1,6 @@
+Feature: User should be able to login to Wytrack App
+
+  Scenario: Driver shoud be able to login
+    Given the user is on the login page
+    When the user enters the driver information
+    Then the user sould be able to login
